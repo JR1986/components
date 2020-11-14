c588b7be66bee8d01c13004deb6a148c4de1b880
Jelle <jelle@fashionunited.com>
2020-11-14 22:07:22 +0100

    Add button

---
+++ components/Button/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MuiButton from '@material-ui/core/Button';
+
+const Button = ({ ...props }) => (
+  <MuiButton
+    {...props}
+  />
+);
+
+export default Button;
