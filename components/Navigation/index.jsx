b1d9a13f8c38507f7bc1b5f17a6e0a3d40a995bf
Jelle <jelle@fashionunited.com>
2020-12-10 22:20:22 +0100

    Add emotion styled components

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-export default function Navigation() {
+const StyledToolbar = styled(Toolbar)`
+  justify-content: flex-end;
+`;
+
+const StyledTypography = styled(Typography)`
+  padding: 0 16px;
+`;
+
+export default function Navigation({ links }) {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Box clone m={2}>
-          <Typography variant="h6">
-            Home
-          </Typography>
-        </Box>
-        <Box clone m={2}>
-          <Typography variant="h6">
-            About
-          </Typography>
-        </Box>
-        <Box clone m={2}>
-          <Typography variant="h6">
-            Services
-          </Typography>
-        </Box>
-        <Box clone m={2}>
-          <Typography variant="h6">
-            Contact
-          </Typography>
-        </Box>
-      </Toolbar>
+      <StyledToolbar>
+        {links.map((link) => (
+          <StyledTypography variant="h6">
+            {link}
+          </StyledTypography>
+        ))}
+      </StyledToolbar>
     </AppBar>
   );
 }
+
+Navigation.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      link: PropTypes.node,
+    }),
+  ),
+};
+
+Navigation.defaultProps = {
+  links: null,
+};
